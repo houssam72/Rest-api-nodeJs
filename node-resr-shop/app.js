d711c6ef4803527818e0b8e50e1e2ec2fb3a3b0a
houssam72 <brothersmarket72@gmail.com>
2023-05-11 16:00:41 +0200

    Add BodyParser and fixing cors error

--- node-resr-shop/app.js
+++ node-resr-shop/app.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
+const app = express();
 const productRouter = require("./api/routes/products");
 const orderRouter = require("./api/routes/orders");
-const { error } = require("console");
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/products", productRouter);
 app.use("/orders", orderRouter);
