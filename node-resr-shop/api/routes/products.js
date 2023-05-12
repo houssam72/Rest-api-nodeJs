9536c87ddd9238208e079e3e2db97829422fce81
houssam72 <brothersmarket72@gmail.com>
2023-05-12 02:01:29 +0200

    Add Mongoose validation

--- node-resr-shop/api/routes/products.js
+++ node-resr-shop/api/routes/products.js
@@ -5,11 +5,26 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id")
     .exec()
     .then((docs) => {
       if (docs) {
+        const response = {
+          count: docs.length,
+          products: docs.map((doc) => {
+            return {
+              name: doc.name,
+              price: doc.price,
+              _id: doc._id,
+              request: {
+                type: "GET",
+                url: "http://localhost:3000/products/" + doc._id,
+              },
+            };
+          }),
+        };
         console.log("get all products", docs);
-        res.status(200).json(docs);
+        res.status(200).json(response);
       }
     })
     .catch((err) => {
@@ -27,27 +42,39 @@ router.post("/", (req, res, next) => {
   product
     .save()
     .then((result) => {
-      console.log("Post a Product to a dbb", result);
       res.status(201).json({
-        message: "Hanling post request to /Product",
-        createdProduct: result,
+        message: "Created product successfully",
+        createdProduct: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result._id,
+          },
+        },
       });
     })
     .catch((err) => {
-      console.log("ErrDbb1", err);
+      res.status(404).json(err);
     });
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then((doc) => {
       if (doc) {
-        console.log("Find by Id a product in a dbb is Ok", doc);
-        res.status(200).json(doc);
+        res.status(200).json({
+          product: doc,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products",
+          },
+        });
       } else {
-        console.log("Find by Id a product in a dbb is Ko", err);
         res.status(500).json({ error: err });
       }
     })
@@ -65,8 +92,13 @@ router.patch("/:productId", (req, res, next) => {
   Product.updateMany({ _id: id }, req.body)
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/products/" + id,
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
@@ -81,7 +113,17 @@ router.delete("/:productId", (req, res, next) => {
   Product.deleteOne({ _id: id })
     .exec()
     .then((result) => {
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product deleted",
+        request: {
+          type: "POST",
+          request: {
+            type: "POST",
+            url: "http://localhost:3000/products/",
+            body: { name: "String", price: "Number" },
+          },
+        },
+      });
     })
     .catch((err) => {
       console.log(err);
